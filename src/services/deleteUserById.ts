dd1e3873af2f9cae0748967ba246e5cb2a6c3231
João Brentano <joaobrentano.br@gmail.com>
2021-09-24 23:39:46 -0300

    Spacing

--- src/services/deleteUserById.ts
+++ src/services/deleteUserById.ts
@@ -13,15 +13,18 @@ export const deleteUserById = async (
 			message: ErrorMessages.NOT_AUTHORIZED_USER,
 		});
 	}
+
 	if (id.length != 36)
 		return response.status(StatusCodes.BAD_REQUEST).json({
 			message: id + ErrorMessages.NOT_ALLOWED_PATTERN,
 		});
+
 	const foundUser = await keycloakAdminClient.users.findOne({ id });
 	if (foundUser == null)
 		return response.status(StatusCodes.NOT_FOUND).json({
 			message: ErrorMessages.NO_RECORDS_FOUND,
 		});
+
 	try {
 		await keycloakAdminClient.users.del({ id });
 		return response.status(StatusCodes.OK).json({
