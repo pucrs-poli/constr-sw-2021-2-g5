f60dfdec07fecb0607ceb425b8e2dc2985a3890a
soares-eduardo <eduardoandre.mcc@gmail.com>
2021-10-03 15:10:41 -0300

    Reviwed my code style.

--- src/services/createUser.ts
+++ src/services/createUser.ts
@@ -4,35 +4,35 @@ import { StatusCodes } from "http-status-codes";
 import { ErrorMessages } from "../utils/response-messages";
 
 export const createUser = async (
-	request: Request,
-	response: Response
+    request: Request,
+    response: Response
 ): Promise<Response> => {
-	let { username, email, firstName, lastName, emailVerified, enabled } =
-		request.body;
+    let { username, email, firstName, lastName, emailVerified, enabled } =
+        request.body;
 
-	try {
-		if (!keycloakAdminClient.accessToken) {
-			return response.status(StatusCodes.BAD_REQUEST).json({
-				message: ErrorMessages.NOT_AUTHORIZED_USER,
-			});
-		}
+    if (keycloakAdminClient.accessToken) {
+        try {
+            const res = await keycloakAdminClient.users.create({
+                username,
+                email,
+                firstName,
+                lastName,
+                emailVerified,
+                enabled,
+            });
 
-		const res = await keycloakAdminClient.users.create({
-			username,
-			email,
-			firstName,
-			lastName,
-			emailVerified,
-			enabled,
-		});
+            return response.status(StatusCodes.CREATED).json({
+                id: res.id,
+                body: request.body,
+            });
+        } catch (err) {
+            return response.status(StatusCodes.BAD_REQUEST).json({
+                message: err,
+            });
+        }
+    }
 
-		return response.status(StatusCodes.CREATED).json({
-			id: res.id,
-			body: request.body,
-		});
-	} catch (err) {
-		return response.status(StatusCodes.BAD_REQUEST).json({
-			message: err,
-		});
-	}
+    return response.status(StatusCodes.BAD_REQUEST).json({
+        message: ErrorMessages.NOT_AUTHORIZED_USER,
+    });
 };
