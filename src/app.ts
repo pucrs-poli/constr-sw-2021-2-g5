033c3862c2d5ad257c8405ce4d7a1ce6ba5757b8
soares-eduardo <eduardoandre.mcc@gmail.com>
2021-10-24 02:35:13 -0300

    Added Swagger.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express"
+
 import { router } from "./routes";
 
-const app = express();
+import swaggerDocs from "./swagger.json";
+
+const app = express();  
 
 app.use(express.json());
 app.use(router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 export { app };
